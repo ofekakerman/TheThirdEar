de7b8b880d7dd9da2f6ec669ee88084e452d4511
orisassoon <ori.sassoon@gmail.com>
2023-05-24 04:42:24 +0300

    Update main_analyze.py

--- SentencesAnalysis/main_analyze.py
+++ SentencesAnalysis/main_analyze.py
@@ -21,6 +21,7 @@ def run_analyse(conversation,
     train_objects = [violence_train_object, warning_train_object, general_train_object]
 
     prompt = get_promt(system_promt_object, train_objects, conversation_object)
+    print ('analyse..')
     ans = analyse(prompt)
     print(ans)
     return ans
